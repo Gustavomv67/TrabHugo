6dedac3fffd585252eadfb19f19178377c119f5c
Gustavomv67 <gustavomv67@gmail.com>
2019-04-06 20:36:04 -0300

    Adicionar arquivos de projeto.

---
+++ TrabHugo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrabHugo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
